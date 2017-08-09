92e1ed8699d3faa6819aaa00bc2d387d2d3c16ff
Pedro Chambino <pchambino@gmail.com>
2017-08-09 20:44:47 +0100

    High card, not highest card

--- test/pokerwars/winning_hand_test.exs
+++ test/pokerwars/winning_hand_test.exs
@@ -3,19 +3,19 @@ defmodule Pokerwars.Hand.WinningTest do
   import Pokerwars.TestHelpers
   doctest Pokerwars.Hand
 
-  test "pair wins highest card (right)" do
+  test "pair wins high card (right)" do
     assert_winning_hands(["2s 3h 4c 7s Qd", "2s 3h 4c 7s 7d"], ["2s 3h 4c 7s 7d"])
   end
 
-  test "pair wins highest card (left)" do
+  test "pair wins high card (left)" do
     assert_winning_hands(["2s 3h 4c 7s 7d", "2s 3h 4c 7s Qd"], ["2s 3h 4c 7s 7d"])
   end
 
-  test "two pairs wins pair and highest card" do
+  test "two pairs wins pair and high card" do
     assert_winning_hands(["2s 3h 4c 7s 7d", "2s 3h 4c 7s Kd", "2s 4h 4c 7s 7d"], ["2s 4h 4c 7s 7d"])
   end
 
-  test "two pairs wins two highest card hands" do
+  test "two pairs wins two high card hands" do
     assert_winning_hands(["2s 3h 4c 7s Qd", "2s 3h 4c 7s Kd", "2s 3h 4c 7s 7d"], ["2s 3h 4c 7s 7d"])
   end
 end
